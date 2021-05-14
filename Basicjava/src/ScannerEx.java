fb7aeb6c82a1b6c60b67a0aca0308a3d2078bca8
JongHyuckKim <kasiruske@naver.com>
2021-05-14 19:52:33 +0900

    깃연동 테스트

---
+++ Basicjava/src/ScannerEx.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ScannerEx {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("이름, 도시, 나이, 체중, 독신 여부를 빈칸으로"
+				+ " 분리하여 입력하세요.");
+		
+		String name = sc.next();//문자열 읽기
+		System.out.print("이름은 " + name + ", ");
+		
+		String city = sc.next();//문자열 읽기
+		System.out.print("도시는 " + city + ", ");
+		
+		int age = sc.nextInt();
+		System.out.print("나이는 " + age + "살, ");
+		
+		double weight = sc.nextDouble();
+		System.out.print("체중은 " + weight + "kg, ");
+		
+		boolean isSingle = sc.nextBoolean();
+		System.out.print("독신여부는 " + isSingle + "입니다.");
+		
+		
+		
+	}
+
+}
